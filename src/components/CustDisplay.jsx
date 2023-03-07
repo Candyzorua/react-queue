3f68623ac820f5f183542e3cc6087a2fb4cff6ad
Candyzorua <jiaying.ong2@gmail.com>
2023-03-07 00:16:24 -0800

    Reduced redundant dependency on queue

--- src/components/CustDisplay.jsx
+++ src/components/CustDisplay.jsx
@@ -8,9 +8,9 @@ function CustDisplay(props) {
 
     return (
         <div className="cust-display">
-            {(props.latestServingNum === 0) ? <h1>Now Serving: --</h1> : <h1>Now Serving: {props.latestServingNum}</h1>}
-            <h2>Last Number: {(props.lastIssuedNum === 0) ? "--" : props.lastIssuedNum}</h2>
-            <button onClick={props.fn}>Take a number</button>
+            {(props.currNum === 0) ? <h1>Now Serving: --</h1> : <h1>Now Serving: {props.currNum}</h1>}
+            <h2>Last Number: {(props.latestNum === 0) ? "--" : props.latestNum}</h2>
+            <button onClick={props.addCust}>Take a number</button>
         </div>
     );
 }
